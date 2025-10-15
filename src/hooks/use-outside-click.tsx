3fcb3742cc865666a2c1c34208edf1eb2a1678ad
teksite <teksite.design@gmail.com>
2025-10-15 08:44:43 +0330

    minor ui change

--- src/hooks/use-outside-click.tsx
+++ src/hooks/use-outside-click.tsx
@@ -1,24 +1,21 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 
 export const useOutsideClick = (
-  ref: React.RefObject<HTMLDivElement>,
-  callback: Function
+    ref: React.RefObject<HTMLDivElement | null>,
+    callback: (event: MouseEvent | TouchEvent) => void
 ) => {
-  useEffect(() => {
-    const listener = (event: any) => {
-      // DO NOTHING if the element being clicked is the target element or their children
-      if (!ref.current || ref.current.contains(event.target)) {
-        return;
-      }
-      callback(event);
-    };
+    useEffect(() => {
+        const listener = (event: MouseEvent | TouchEvent) => {
+            if (!ref.current || ref.current.contains(event.target as Node)) return;
+            callback(event);
+        };
 
-    document.addEventListener("mousedown", listener);
-    document.addEventListener("touchstart", listener);
+        document.addEventListener("mousedown", listener);
+        document.addEventListener("touchstart", listener);
 
-    return () => {
-      document.removeEventListener("mousedown", listener);
-      document.removeEventListener("touchstart", listener);
-    };
-  }, [ref, callback]);
+        return () => {
+            document.removeEventListener("mousedown", listener);
+            document.removeEventListener("touchstart", listener);
+        };
+    }, [ref, callback]);
 };
